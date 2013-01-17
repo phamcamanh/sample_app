39df3094a0270bf81dc94731a0824aee0c19a43e
phamcamanh <pham.cam.anh@framgia.com>
2013-01-17 05:01:05 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '126392f125fabcc974e45459c569d84b75cdcf1b9d277589e851ce24640d327b852b90075e5f37cc9e98253e88031fd3b6e025de57163afa3a0bf416a0820781'
